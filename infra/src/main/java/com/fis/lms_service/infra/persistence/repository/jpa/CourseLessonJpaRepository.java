268e3d78557624013381b93701bc5f64a119ae10
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2026-01-29 11:33:25 +0700

    spotless apply

--- infra/src/main/java/com/fis/lms_service/infra/persistence/repository/jpa/CourseLessonJpaRepository.java
+++ infra/src/main/java/com/fis/lms_service/infra/persistence/repository/jpa/CourseLessonJpaRepository.java
@@ -5,10 +5,7 @@ import org.jspecify.annotations.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- * Admin 1/26/2026
- */
+/** Admin 1/26/2026 */
 @Repository
 public interface CourseLessonJpaRepository
-        extends JpaRepository<@NonNull CourseLessonEntity, @NonNull Long> {
-}
+    extends JpaRepository<@NonNull CourseLessonEntity, @NonNull Long> {}
--- infra/src/main/java/com/fis/lms_service/infra/persistence/repository/jpa/LessonJpaRepository.java
+++ infra/src/main/java/com/fis/lms_service/infra/persistence/repository/jpa/LessonJpaRepository.java
@@ -5,9 +5,6 @@ import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- * Admin 1/26/2026
- */
+/** Admin 1/26/2026 */
 @Repository
-public interface LessonJpaRepository extends JpaRepository<@NonNull LessonEntity, @NonNull Long> {
-}
+public interface LessonJpaRepository extends JpaRepository<@NonNull LessonEntity, @NonNull Long> {}
