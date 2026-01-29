903007e9dea563b459192e42be01515235e92075
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2026-01-29 11:52:01 +0700

    add mapper

---
+++ infra/src/main/java/com/fis/lms_service/infra/persistence/mapper/LessonMapper.java
@@ -0,0 +1,16 @@
+package com.fis.lms_service.infra.persistence.mapper;
+
+import com.fis.lms_service.core.domain.model.lesson.LessonModel;
+import com.fis.lms_service.infra.persistence.entity.lesson.LessonEntity;
+import org.mapstruct.Mapper;
+
+/**
+ * Admin 1/29/2026
+ *
+ **/
+@Mapper(componentModel = "spring")
+public interface LessonMapper {
+    LessonEntity toEntity(LessonModel model);
+
+    LessonModel toModel(LessonEntity entity);
+}
